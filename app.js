1cfa56a5657044cd0fd2ab85c44deb17762b6c22
unlimitedabby <165395865+unlimitedabby@users.noreply.github.com>
2024-03-29 14:43:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_ycnkmx9", "template_wap36p5", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
